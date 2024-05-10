9986ef1868d8c004fe29a5a26a8e96714a3f1b84
AaronSoftwareDev <sichone1956@gmail.com>
2024-05-10 16:39:09 +0200

    first commit

---
+++ financial_app/src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import Button from "primevue/button"
+</script>
+
+<template>
+
+
+<div class="card flex justify-content-center">
+        <Button label="Check" icon="pi pi-check" />
+    </div>
+
+
+</template>
